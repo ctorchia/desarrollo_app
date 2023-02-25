274349a195d5422a62978ed2a22e139ecec04672
DESKTOP-V2BVTP7\CMT <cristian_torchia@yahoo.com.ar>
2023-02-25 12:10:57 -0300

    Desafio 02: Base

--- desafio_02/App.js
+++ desafio_02/App.js
@@ -1,30 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { ListItem, NewItemHeader, Modal } from "./src/components";
 
 export default function App() {
+  const [itemText, setItemText] = useState("");
+  const [items, setItems] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const onChangeText = (text) => {
+    setItemText(text);
+  };
+
+  useEffect(() => {
+    console.log("useEffect", "itemText", itemText, "ITEMS", items);
+  }, []);
+
+  const addItemToState = () => {
+    console.log("addItemToState - start SIN JSON", items, itemText);
+    console.log(
+      "addItemToState - start CON JSON",
+      JSON.stringify({ items, itemText })
+    );
+    const newArr = [...items, { id: Date.now(), value: itemText }];
+    setItems(newArr);
+    setItemText("");
+    console.log("addItemToState - end", "items", newArr);
+  };
+
+  const openModal = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+  };
+
+  const onCancelModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  const onDeleteModal = (id) => {
+    setModalVisible(!modalVisible);
+    setItems((oldArry) => oldArry.filter((item) => item.id !== id));
+    setSelectedItem(null);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.mainText}>Mi primer App!</Text>
-      <Text style={styles.descriptionText}>(Cristian Torchia)</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      {/* ADDITEM COMPONENT */}
+      <NewItemHeader
+        onChangeText={onChangeText}
+        itemText={itemText}
+        addItemToState={addItemToState}
+      />
+      {/* LIST COMPONENT */}
+      <ListItem items={items} openModal={openModal} />
+      {/* MODAl COMPONENT */}
+      <Modal
+        modalVisible={modalVisible}
+        selectedItem={selectedItem}
+        onCancelModal={onCancelModal}
+        onDeleteModal={onDeleteModal}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
+    padding: 30,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  mainText: { 
-    fontSize: 30, 
-    color: 'red' 
-  },
-  descriptionText: { 
-    fontSize: 20, 
-    color: 'blue' 
   },
 });
