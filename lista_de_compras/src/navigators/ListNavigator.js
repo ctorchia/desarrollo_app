21b850dab443fdfc96c01e5ffddda27def65b727
ctorchia <cristian_torchia@yahoo.com.ar>
2023-04-17 16:52:35 -0300

    TopTabNavigator para Listas "A COMPRAR" y "YA COMPRADOS"

--- lista_de_compras/src/navigators/ListNavigator.js
+++ lista_de_compras/src/navigators/ListNavigator.js
@@ -1,36 +1,130 @@
-import { Platform, StyleSheet, Text, View } from 'react-native'
+import * as React from 'react';
 
-import { COLORS } from '../constants/colors'
+import { Text, View } from 'react-native';
+
+import { Ionicons } from '@expo/vector-icons';
 import ListScreen from '../screens/ListScreen'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-const Stack = createNativeStackNavigator();
+// import { NavigationContainer } from '@react-navigation/native';
 
-const ListNavigator = () => {
+
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Home!</Text>
+//     </View>
+//   );
+// }
+
+function SettingsScreen() {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: Platform.OS === 'android' ? COLORS.primary : ''
-        },
-        headerTintColor: Platform.OS === 'android' ? 'white' : COLORS.primary,
-        headerTitleStyle: {
-          fontFamily: 'OpenSans_700Bold'
-        }
-      }}
-    >
-      <Stack.Screen
-        name="Cart"
-        component={ListScreen}
-        options={{
-          title: 'Lista de compras'
-        }}
-      />
-    </Stack.Navigator>
-  )
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
 }
 
-export default ListNavigator
+const Tab = createMaterialTopTabNavigator();
 
-const styles = StyleSheet.create({})
+export default function App() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === 'Home') {
+            return (
+              <Ionicons
+                name={
+                  focused
+                    ? 'ios-information-circle'
+                    : 'ios-information-circle-outline'
+                }
+                size={size}
+                color={color}
+              />
+            );
+          } else if (route.name === 'Settings') {
+            return (
+              <Ionicons
+                name={focused ? 'ios-list-box' : 'ios-list'}
+                size={size}
+                color={color}
+              />
+            );
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="A COMPRAR" component={ListScreen} />
+      <Tab.Screen name="YA COMPRADOS" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Platform, StyleSheet, Text, View } from 'react-native'
+
+// import { COLORS } from '../constants/colors'
+// import ListScreen from '../screens/ListScreen'
+// import React from 'react'
+// import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+// const Stack = createNativeStackNavigator();
+
+// const ListNavigator = () => {
+//   return (
+//     <Stack.Navigator
+//       screenOptions={{
+//         headerStyle: {
+//           backgroundColor: Platform.OS === 'android' ? COLORS.primary : ''
+//         },
+//         headerTintColor: Platform.OS === 'android' ? 'white' : COLORS.primary,
+//         headerTitleStyle: {
+//           fontFamily: 'OpenSans_700Bold'
+//         }
+//       }}
+//     >
+//       <Stack.Screen
+//         name="Cart"
+//         component={ListScreen}
+//         options={{
+//           title: 'Lista de compras'
+//         }}
+//       />
+//     </Stack.Navigator>
+//   )
+// }
+
+// export default ListNavigator
+
+// const styles = StyleSheet.create({})
